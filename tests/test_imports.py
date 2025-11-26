e0b81e329d7c7eabe4838e00a520bb7a95a19123
utmtansenghooi <tansenghooi@graduate.utm.my>
2025-11-26 16:41:29 +0000

    Remove Btree import test

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -9,8 +9,6 @@ def test_import_modules():
         "src.pos_system.inventory",
         "src.pos_system.inventory.binary_search_tree",
         "src.pos_system.inventory.splay_tree",
-        "src.pos_system.sales",
-        "src.pos_system.sales.btree",
         "src.pos_system.loyalty",
         "src.pos_system.loyalty.avl_tree",
     ]
